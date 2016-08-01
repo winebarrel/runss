3ab6b9d7171ef6bd0099f9a6645b22e0472c2866
winebarrel <sgwr_dts@yahoo.co.jp>
2016-08-02 00:36:49 +0900

    Update Formula [ci skip]

--- homebrew/runss.rb
+++ homebrew/runss.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Runss < Formula
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   homepage 'https://github.com/winebarrel/runss'
   url "https://github.com/winebarrel/runss/releases/download/v#{VERSION}/runss-v#{VERSION}-darwin-amd64.gz"
-  sha256 '2562f8aca1eb576f9286f7ec0a59d27012ff230824f72e273e21a8614dbd4e72'
+  sha256 '7687a68b775cbca81b3f5ba4917bb70d4279eb494d5b2ae3a9f6da7be05bad82'
   version VERSION
   head 'https://github.com/winebarrel/runss.git', :branch => 'master'
 
